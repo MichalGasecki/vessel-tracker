0753aa47cfa3dbf806533693eee967ae1ec7c966
Michał Gąsecki <michal.gasecki@gmail.com>
2019-10-11 22:40:53 +0200

    First version

---
+++ VesselTracker Project/VesselTracker/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using DbUp;
+using DbUp.Helpers;
+
+namespace VesselTracker
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString =
+                args.FirstOrDefault()
+                ?? "Server=(local)\\SqlExpress; Database=VesselTracker; Trusted_connection=true";
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .JournalTo(new NullJournal())
+                    .LogToConsole()
+                    .WithExecutionTimeout(TimeSpan.FromSeconds(180))
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                Console.ReadLine();
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
